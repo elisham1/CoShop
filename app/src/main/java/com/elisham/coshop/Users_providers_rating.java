b173d51c0a3446bf647d4a000b13649d694b7fe1
yaelBorochov <8488274@gmail.com>
2024-03-13 16:57:01 +0200

    Menu and more pages

--- app/src/main/java/com/elisham/coshop/Users_providers_rating.java
+++ app/src/main/java/com/elisham/coshop/Users_providers_rating.java
@@ -5,31 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.content.Intent;
 import android.widget.Toast;
 
-public class HomePageActivity extends AppCompatActivity {
-
-    public Button filterButton;
-    public void init() {
-        filterButton = (Button) findViewById(R.id.filterButton);
-        filterButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent toy = new Intent(HomePageActivity.this, FilterActivity.class);
-                startActivity(toy);
-            }
-        });
-    }
+public class Users_providers_rating extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
-        init();
+        setContentView(R.layout.activity_users_providers_rating);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
