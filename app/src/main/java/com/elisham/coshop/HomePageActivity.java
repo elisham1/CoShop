e76ac1524c25caad411adf7b2cc8fcc8513272c0
yaelBorochov <8488274@gmail.com>
2024-03-13 17:50:25 +0200

    Change one page

--- app/src/main/java/com/elisham/coshop/HomePageActivity.java
+++ app/src/main/java/com/elisham/coshop/HomePageActivity.java
@@ -3,13 +3,11 @@ package com.elisham.coshop;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import android.view.Menu;
-
 import android.view.MenuItem;
 import android.view.View;
-import android.content.Intent;
 import android.widget.Toast;
 
 public class HomePageActivity extends AppCompatActivity {
@@ -25,18 +23,6 @@ public class HomePageActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        // Handle the back button click
-        if (id == android.R.id.home) {
-            onBackPressed(); // Go back when the back arrow is clicked
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,10 +30,12 @@ public class HomePageActivity extends AppCompatActivity {
         return true;
     }
 
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed(); // Go back when the back arrow is clicked
+                return true;
             case R.id.Personal_info:
                 Toast.makeText(this, "Personal Info is clicked", Toast.LENGTH_SHORT).show();
                 return true;
@@ -81,6 +69,4 @@ public class HomePageActivity extends AppCompatActivity {
         Intent toy = new Intent(HomePageActivity.this, OpenNewOrderActivity.class);
         startActivity(toy);
     }
-
-
 }
